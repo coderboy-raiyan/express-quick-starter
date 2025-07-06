2c5455d641a4654f07c6dacc2ecbb0c46fd762e6
Raiyan <tajkierhaque@gmail.com>
2025-07-06 14:23:56 +0600

    some issues has been fixed

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import http from "http";
 import config from "./config";
 
 const app = express();
 const server = http.createServer(app);
 
-console.log("hello");
+app.get("/", (req: Request, res: Response) => {
+    return res.status(200).json({ message: "Server is healthy" });
+});
 
 server.listen(config.port, () => {
     console.log("Server is listening on PORT " + config.port);
